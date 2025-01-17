d50005921da072c0b5d4369ae619c85817c39ca9
wr4ma <111068847+wr4ma@users.noreply.github.com>
2025-01-17 18:08:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$("#menu").click(function(){
+  $("#mainmenu").toggleClass("showmenu");
+  
+  $("#mainscreen").toggleClass("movemaincontainer");
+});
+
+
+
+$("#menu").click(function(){
+ $("#mainmenu2").toggleClass("showmenu2");
+  $("#mainscreen").toggleClass("movemaincontainer");
+});
